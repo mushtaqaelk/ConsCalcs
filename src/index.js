f71da7f025ac388d71ab407f4fb9f3af59351463
albajawee <albajawee@gmail.com>
2025-06-27 20:48:16 +0300

    adding the tailwind adn postcss

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css'; // <-- ADD THIS LINE
 import App from './App';
-
-// This is the standard entry point for a React application.
-// It finds the 'root' div in your public/index.html file and
-// renders the main App component into it.
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
