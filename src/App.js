9664fa74c4f2373f1a91a163bbe979498a1f19ed
Mushtaq Kareem <mushtaqaelk@gmail.com>
2025-07-08 01:23:02 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -373,9 +373,18 @@ const ResearchPage = () => {
     const [copySuccess, setCopySuccess] = useState('');
 
     const citations = {
-        APA: "Researcher, A. (2025). The index of disagreement: A novel framework for consensus measurement. Journal of Advanced Quantitative Methods, 15(2), 123-145. https://doi.org/10.xxxx/j.jaqm.2025.xx.xxx",
-        MLA: 'Researcher, Anonymous. "The Index of Disagreement: A Novel Framework for Consensus Measurement." Journal of Advanced Quantitative Methods, vol. 15, no. 2, 2025, pp. 123-45, doi:10.xxxx/j.jaqm.2025.xx.xxx.',
-        BibTeX: `@article{researcher2025index,\n  title={The Index of Disagreement: A Novel Framework for Consensus Measurement},\n  author={Researcher, Anonymous},\n  journal={Journal of Advanced Quantitative Methods},\n  volume={15},\n  number={2},\n  pages={123--145},\n  year={2025},\n  publisher={Academic Press},\n  doi={10.xxxx/j.jaqm.2025.xx.xxx}\n}`
+        APA: "Akiyama, Y., Nolan, J., Darrah, M., Abdal Rahem, M., & Wang, L. (2016). A method for measuring consensus within groups: An index of disagreement via conditional probability. Information Sciences, 345, 116–128. https://doi.org/10.1016/j.ins.2016.01.052",
+        MLA: 'Akiyama, Y., et al. "A Method for Measuring Consensus Within Groups: An Index of Disagreement Via Conditional Probability." Information Sciences, vol. 345, 2016, pp. 116–128. https://doi.org/10.1016/j.ins.2016.01.052',
+        BibTeX: `@article{akiyama2016method,
+  title={A Method for Measuring Consensus Within Groups: An Index of Disagreement Via Conditional Probability},
+  author={Akiyama, Y. and Nolan, J. and Darrah, M. and Abdal Rahem, M. and Wang, L.},
+  journal={Information Sciences},
+  volume={345},
+  pages={116--128},
+  year={2016},
+  doi={10.1016/j.ins.2016.01.052}
+}
+`
     };
 
     const copyToClipboard = (text) => {
